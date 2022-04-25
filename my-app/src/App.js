a9f3c1fe73f900571a767a50e96bb6576955b7b6
MalinKirjonen <91899355+MalinKirjonen@users.noreply.github.com>
2022-04-25 18:19:47 +0200

    Initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import faceMalin from "./faceMalin.JPG";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={faceMalin} alt="logo" className=" App-logo img-fluid" />
+      </header>
+    </div>
+  );
+}
