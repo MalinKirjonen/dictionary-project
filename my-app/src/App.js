100b667b708dcc22fa629399172ac337c8d44b49
MalinKirjonen <91899355+MalinKirjonen@users.noreply.github.com>
2022-04-27 15:51:05 +0200

    Created useState search function

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,21 @@
 import faceMalin from "./faceMalin.JPG";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={faceMalin} alt="logo" className=" App-logo img-fluid" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={faceMalin} alt="logo" className=" App-logo img-fluid" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Malin Kirjonen</small>
+        </footer>
+      </div>
     </div>
   );
 }
